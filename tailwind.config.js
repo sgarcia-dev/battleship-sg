830ec968812f46391fe46fea23f9ac7b1755d804
Sergei Garcia <sgarcia.dev@gmail.com>
2023-10-20 16:24:09 -0600

    chore: clean up file structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './*.html',
+    './src/**/*.html',
+    './src/**/*.jsx',
+    './src/**/*.js',
+    './src/**/*.tsx',
+    './src/**/*.ts',
+  ],
   theme: {
     extend: {},
   },
